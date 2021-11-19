0399e65d0ffe42c1c6c5f9eb6e7e1b463e5564d7
Hauryliuk-Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2021-11-19 22:28:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+function showBurger(){
+    const elem = document.getElementsByClassName('burger');
+    const elem2 = document.getElementsByClassName('menu__list');
+    elem.addEventListener('click', function(){
+        if(elem2.style.displey == "none"){
+            elem2.style.displey = "block"
+        }
+        else{
+            elem2.style.displey = "none"
+        }
+    })
+}
+showBurger();
+
+//please, help...
 
